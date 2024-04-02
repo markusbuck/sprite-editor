1136eb65869647830d93fd43c73d867c63a08c0a
frosty <57603739+frostyio@users.noreply.github.com>
2024-04-01 20:59:12 -0600

    help dialog finishing touches

--- helpdialog.h
+++ helpdialog.h
@@ -3,16 +3,35 @@
 
 #include <QDialog>
 
+/*!
+ * Authors: Kevin Soto, Zak Holt, Walker Hall, Brenden Suess, and Markus Buckwalter
+ * CS 3505 Assignment 8: Sprite Editor Implementation
+ * Date: 4/1/24
+ *
+ * This file contains the components that control the help dialog window of the sprite editor.
+ */
 namespace Ui {
 class HelpDialog;
 }
 
+
+/*!
+ * \brief The HelpDialog class
+ */
 class HelpDialog : public QDialog
 {
     Q_OBJECT
 
 public:
+    /*!
+     * \brief HelpDialog - Constructor for HelpDialog.
+     * \param parent - null pointer.
+     */
     explicit HelpDialog(QWidget *parent = nullptr);
+
+    /*!
+     * \brief ~HelpDialog - Deconstructor for HelpDialog.
+     */
     ~HelpDialog();
 
 private:
